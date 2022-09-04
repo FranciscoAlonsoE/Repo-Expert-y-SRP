06c94e13a56ec479de48ea9e80a41683df4d58df
FranciscoAlonsoE <francisco.alonsoe@correo.ucu.edu.uy>
2022-09-04 20:12:50 -0300

    Ejercicio1

---
+++ Ejercicio 1/src/Library/Libreria.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SRP
+{
+    public class Library {
+        public string LibrarySector { get ; set; }
+        public string LibraryShelve { get ; set; }
+
+        public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+    }
+}
